1761e400115847166a5687ede5c8e70dda073501
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2022-05-09 23:54:08 +0200

    Added links file to change routes at a single place
    * is still WIP, all files need to use the links/routes of this page

---
+++ lib/links.ts
@@ -0,0 +1,12 @@
+export default function getLinks() {
+    return {
+        "root": "http://localhost:3000",
+        "user": {
+            "login": "/auth/login",
+            "register": "/auth/register"
+        },
+        "api": {
+            
+        }
+    };
+}
