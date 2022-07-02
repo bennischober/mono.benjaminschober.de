faa146ccce6b7556a3fe8a714197e009445a9e70
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2022-07-02 14:54:02 +0200

    Added quick sort

---
+++ components/SearchAlgorithms/QuickSort/index.tsx
@@ -0,0 +1,58 @@
+import { useEffect } from "react";
+import { Bar, QuickSortProps } from "../../../types/interfaces";
+
+export function QuickSort({
+    bars,
+    speed,
+    setBars,
+    task,
+    done,
+}: QuickSortProps) {
+    useEffect(() => {
+        if (typeof window === "undefined") return;
+        quicksort();
+    }, []);
+
+    const quicksort = async () => {
+        if (!bars) {
+            console.log("bars are empty!");
+            return;
+        }
+
+        let currentArr = bars;
+
+        await sort(currentArr, 0, currentArr.length - 1);
+        done("Quick sort finished!");
+    };
+
+    const sort = async (A: Bar[], p: number, r: number) => {
+        if (p < r) {
+            let q = await partition(A, p, r);
+            await task(speed);
+            setBars([...A]);
+            await sort(A, p, q - 1);
+            await sort(A, q + 1, r);
+        }
+    };
+
+    const partition = async (
+        A: Bar[],
+        p: number,
+        r: number
+    ): Promise<number> => {
+        let x = A[r];
+        let i = p - 1;
+        for (let j = p; j < r; j++) {
+            if (A[j].key <= x.key) {
+                i++;
+                [A[i], A[j]] = [A[j], A[i]];
+                setBars([...A]);
+                await task(speed);
+            }
+        }
+        [A[i + 1], A[r]] = [A[r], A[i + 1]];
+        return i + 1;
+    };
+
+    return <></>;
+}
