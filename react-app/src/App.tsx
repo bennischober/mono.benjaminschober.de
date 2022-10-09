0bceae7fa073611d418b2a1639b22643aeacb8a1
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2022-10-10 00:48:52 +0200

    Updated App and removed unused files
    * also added me.jpg

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,28 +1,23 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { Header } from "./components/Header";
+import { LeftSidebar } from "./components/SidebarLeft";
+import { IntroductionSection } from "./sections/Introduction";
+import styled from "styled-components";
+import { AboutSection } from "./sections/About";
+
+const StyledMain = styled.main`
+    padding: 0px 150px;
+`;
 
 function App() {
     return (
         <>
-            <Header>Hi this is a Header</Header>
-            <div style={{ height: "2000vh" }} />
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+            <Header />
+            <LeftSidebar />
+            <div id="content">
+                <StyledMain>
+                    <IntroductionSection />
+                    <AboutSection />
+                </StyledMain>
             </div>
         </>
     );
